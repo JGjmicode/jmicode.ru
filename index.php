9b43443d4acb29cc0d89e01ebf422fd03c75bc2b
JGjmicode <jmi.std@yandex.ru>
2017-05-14 22:29:02 +0300

    Этапы работы

--- index.php
+++ index.php
@@ -61,21 +61,48 @@
                 <h5>Контекстная реклама</h5>
                 <h5>SEO-продвижение</h5>
                 <h5>Услуги комплексного продвижения</h5>
-            </section>
-            
-            <h2>Разработка web-проектов любой сложности, разработка адаптивных сайтов</h2>
-            
+            </section>                                    
         </div>
         
-        <div class="wide">
+        <div class="">
         <div class="process-banner">
             <img src="img/process-baner.jpg" />
         </div>        
         </div>
                 
+        
+        
+        <div class="container">
+            <h2>Разработка web-проектов любой сложности, разработка адаптивных сайтов</h2>
+            <div class="serv clearfix">
+                <section class="process-tab" onclick="servClick('1');">Разработка сайтов</section>
+                <section class="process-tab" onclick="servClick('2');">Анализ</section>
+                <section class="process-tab" onclick="servClick('3');">Дизайн</section>
+                <section class="process-tab" onclick="servClick('4');">Программирование</section>
+                <section class="process-tab" onclick="servClick('5');">Сопровождение</section>
+                <section class="process-tab" onclick="servClick('6');">Продвижение</section>
+                <section class="process-tab" onclick="servClick('7');">Оптимизация</section>
+                <section class="process-tab" onclick="servClick('8');">Реконструкция</section>            
+            </div>
+            <div class="serv-description">
+                <div id="des1">Если Вам необходим качественный и недорогой сайт в сжатые сроки, Вы попали по адресу!</div>
+                <div id="des2" class="serv-description-hide">Без анализа невозможно сделать качественный, полезный и нужный проект.</div>
+                <div id="des3" class="serv-description-hide">Завершив и согласовав с клиентом предыдущие этапы, мы приступаем к дизайну страниц сайта, который соответствует принятой концепции и основан на пожеланиях Заказчика.</div>
+                <div id="des4" class="serv-description-hide">После согласования дизайн-макетов, за дело берутся backend и frontend специалисты, которые воплотят и наполнят жизнью всё то, что было задумано на стадии проектировки и дизайна.</div>
+                <div id="des5" class="serv-description-hide">Мы обеспечиваем стабильную техническую поддержку, безопасность Вашего сайта, модернизацию и оперативное информационное сопровождение.</div>
+                <div id="des6" class="serv-description-hide">Продвижение бывает разным: SEO, контекстная реклама в «Яндекс.Директ» и Google Adwords. С помощью грамотного использования этих инструментов становится возможным успешное продвижение услуг, сайта, компании и бренда в целом.</div>
+                <div id="des7" class="serv-description-hide">Структура сайта. Улучшение навигации, меню и юзабилити в целом. 
+                    Тексты. Уникализация, добавление ключевых слов и структурирование текстов.
+                    Графика. Уменьшение размера графических элементов для ускорения загрузки сайта.
+                    Исходный код. Крайне важно очистить код и исправить его, добавить метатеги.</div>
+                <div id="des8" class="serv-description-hide">Реконструкция сайта подразумевает тщательное исследование его состояния, выявление новых потребностей, отсеивание ненужных функций, подбор и применение новых технологий в дизайне, программировании и контенте.</div>
+            </div>
+        </div>
+        
         <footer>
             <img src="img/jmicode.png" />
         </footer>
+        
         <script src="/js/jquery-3.2.1.min.js"></script>
         <script src="/js/common.js"></script>
     </body>
--- js/common.js
+++ js/common.js
@@ -10,8 +10,42 @@ $(window).scroll(function(){
     });
 });
 
+function servClick(i = 1) {
+    
+    for (var j = 1; j <= $(".serv-description").children().length; j++) {        
+        $("#des"+j).addClass("serv-description-hide");
+    }
+            
+    switch (i) {
+        case "1":
+            $("#des1").removeClass("serv-description-hide");
+            break;
+        case "2":
+            $("#des2").removeClass("serv-description-hide");
+            break;
+        case "3":
+            $("#des3").removeClass("serv-description-hide");
+            break;
+        case "4":
+            $("#des4").removeClass("serv-description-hide");
+            break; 
+        case "5":
+            $("#des5").removeClass("serv-description-hide");
+            break; 
+        case "6":
+            $("#des6").removeClass("serv-description-hide");
+            break; 
+        case "7":
+            $("#des7").removeClass("serv-description-hide");
+            break; 
+        case "8":
+            $("#des8").removeClass("serv-description-hide");
+            break;         
+    }
+}
+
 var slideNow = 1;
-var slideCount = $('#slidewrapper').children().length;
+var slideCount = $("#slidewrapper").children().length;
 var slideInterval = 2500;
 
 $(document).ready( function (){
@@ -22,14 +56,14 @@ $(document).ready( function (){
 
 function nextSlide() {    
     if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-        $('#slidewrapper').css('transform', 'translate(0, 0)');
+        $("#slidewrapper").css("transform", "translate(0, 0)");
         slideNow = 1;
     } else {
-        translateWidth = -$('.viewport').width() * (slideNow);
-        $('#slidewrapper').css({
-            'transform': 'translate(' + translateWidth + 'px, 0)',
-            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        translateWidth = -$(".viewport").width() * (slideNow);
+        $("#slidewrapper").css({
+            "transform": "translate(" + translateWidth + "px, 0)",
+            "-webkit-transform": "translate(" + translateWidth + "px, 0)",
+            "-ms-transform": "translate(" + translateWidth + "px, 0)",
         });
         slideNow++;
     }
